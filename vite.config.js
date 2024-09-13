19c0b0319cd0fe9cdcf6978b5eb5850c438271d5
Nitish3925 <learning@trayistats.com>
2024-09-13 13:43:57 +0530

    u
    u

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/infinity-tech-flame/',  // Updated with your repository name
+ // base: '/infinity-tech-flame/',  // Updated with your repository name
   plugins: [react()],
 })
